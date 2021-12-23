b9b4e178975f83938dae5b39709bffa9e6249065
obj-QED <obj.qed@gmail.com>
2021-12-23 21:21:06 +0200

    path change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import { Component } from 'react';
-import '../../assets//style/app.styles.scss';
-// import VisibilitySensor from 'react-visibility-sensor';
-import img from '../../assets/images/img.png';
-
+import '../assets//style/app.styles.scss';
+import img from '../assets/images/img.png';
 import ReactHlsPlayer from 'react-hls-player';
+// import VisibilitySensor from 'react-visibility-sensor';
+
 
 class HomePage extends Component {
-
+  
   render() {
     return (
       <div className='flex items-center justify-center h-screen'>
@@ -14,7 +14,7 @@ class HomePage extends Component {
           <img src={ img } style={{ 'width': '100%', 'height': 'auto'}}></img>
           <ReactHlsPlayer
             // src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-            src="../../assets/video/video.mov"
+            src={'../assets/video/video.mov'}
             autoPlay={true}
             controls={false}
             width="100%"
