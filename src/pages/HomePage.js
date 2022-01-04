65b4a72ce0a911af3a977c69847893582d19eafd
obj-QED <obj.qed@gmail.com>
2022-01-04 22:06:20 +0200

    work tab (current-no-active)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import '../assets//style/app.styles.scss';
 
 import { Layout } from '../components//Layout';
 
-import AccordionItem from '../components/Accordion';
-import { accordionData } from '../utils/content';
-
+import Tabs from '../components/tabItem';
+import { tabData } from '../utils/content';
 
 
 const HomePage = () => {
-  const [activeIndex, setActiveIndex] = useState(false);
+
   return (
     <Layout>
       <div className='flex items-center justify-center h-screen'>
-        <div className="accordion">
-          {accordionData.map(({ item }, index ) => (
-            <AccordionItem
-              item={item}
-              index={index}
-              onClick={() => {
-                setActiveIndex(index);
-              }}
-            />
-          ))}
-        </div>
+        <Tabs items={tabData} />
       </div>
     </Layout>
   );
