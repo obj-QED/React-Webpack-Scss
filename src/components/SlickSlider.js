db3655dbfd1753f8f949a53cebacd353987939c2
Vladislav <send@valabuev.ru>
2022-01-21 12:39:31 +0400

    slider mobile

--- src/components/SlickSlider.js
+++ src/components/SlickSlider.js
@@ -11,7 +11,32 @@ const SlickSlider = ({ slides }) => {
     autoplaySpeed: 2000,
     slidesToShow: 6,
     slidesToScroll: 1,
-    // variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 460,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ]
   }
 
   return (
