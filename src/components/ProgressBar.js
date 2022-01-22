92185f6066bd42ab005795bcf5e02efe100807c0
obj-QED <obj.qed@gmail.com>
2022-01-22 20:20:35 +0200

    blob:url (Tab video file) & progressbar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,7 +5,11 @@ export default function Progressbar({ done }) {
     <div className={"progressbar w-full h-1 pointer-events-none mt-4"}>
       <div
         className={"progressbar-done h-full"}
-        style={{ width: `${done*100}%` }}
+        style={{
+          width: `${Math.round(done * 100)}%`,
+          transition: `width ${Math.abs(done - 1)}s ease`,
+          transitionDelay: `${Math.abs(done)}s`,
+        }}
       ></div>
     </div>
   );
--- src/components/TabItem.js
+++ src/components/TabItem.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactPlayer from 'react-player'
 
-import Progressbar from './Progressbar';
+import ProgressBar from './ProgressBar';
 
+// TODO: view how problem import scss file
 // Style
 import '../assets/style/tabs.scss'
 
@@ -13,8 +14,45 @@ const TabContent = ({ defaultItem, playing, video, setCurrentProgress, setPlayin
         video = defaultItem.video;
     }
 
-    const videoRef = React.createRef(null);
+    // NOTE: This is a hack to make sure the video plays on tab click and change the url
+    const [isReady, setIsReady] = useState(false);
+    const blobUrl = useRef(null);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    // FIX: changing the url affects the video and changes jerkily
+    useEffect(() => {
+        if (video !== null) {
+            setIsLoading(true);
+            fetch(video.url)
+                .then(res => res.blob())
+                .then(blob => {
+                    blobUrl.current = URL.createObjectURL(blob);
+                    setIsLoading(false);
+                })
+        }
+    }, [video]);
 
+    // HACK: needs to be improved
+    // useEffect(() => {
+    //     if (video.url && !isLoading && video !== null) {
+    //         videoRef.current.seekTo(videoProgress["n" + active], 'fraction');
+    //         playVideo(videoRef);
+    //         const url = video.url;
+    //         const xhr = new XMLHttpRequest();
+    //         xhr.open('GET', url, true);
+    //         xhr.responseType = 'blob';
+    //         xhr.onload = function () {
+    //             if (this.status == 200) {
+    //                 const blob = this.response;
+    //                 blobUrl.current = URL.createObjectURL(blob);
+    //                 setIsReady(true);
+    //             }
+    //         };
+    //         xhr.send();
+    //     }
+    // }, [video.url]);
+
+    const videoRef = React.createRef(null);
     const changeProgress = (videoRef) => {
         const ch = videoProgress;
 
@@ -31,24 +69,7 @@ const TabContent = ({ defaultItem, playing, video, setCurrentProgress, setPlayin
     const playVideo = (videoRef) => {
         videoRef.current.seekTo(videoProgress["n" + active], 'fraction');
     }
-
-    const src = video.url;
-    const [isReady, setIsReady] = useState(false);
-    const blobUrl = useRef(null);
-
-    useEffect(() => {
-        fetch(src)
-            .then((response) => response.blob())
-            .then((blob) => {
-                blobUrl.current = URL.createObjectURL(blob);
-                setIsReady(true);
-            });
-
-        return () => {
-            URL.revokeObjectURL(blobUrl.current);
-        };
-    }, []);
-
+    
     return (
         <div className="tab-content w-10/12 mx-auto">
             <div className="data w-full h-full">
@@ -122,92 +143,86 @@ const tabItems = ({ items }) => {
     return (
         <div className="tab">
             <div className="tab-heading grid grid-cols-4 gap-5 mb-20">
-                {items.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`item flex justify-between
+                {items.map((item, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className={`item flex justify-between
                             ${index === active ? 'active' : ''}
                             ${index === hover || index === active && playing ? 'shadow-lg cursor-pointer border-blueCustom-200' : 'border-gray-800'}
                             `}
-                        onClick={openTab}
-                        onMouseEnter={mEnter}
-                        onMouseLeave={mLeave}
-                        data-index={index}
-                    >
-                        <div className="state" data-index={index}>
-                            <div className={`state__icon cursor-pointer ${videoProgress["n" + index] != 1 && playing && index == active ? 'playing' : ''}`}
-                                data-index={index}>
-                                <div className="state__icon__overlay" data-index={index}>
-                                    <div className="icon" data-index={index}>
-                                        {/* Default Icon */}
-                                        {videoProgress["n" + index] != 1 && index != active &&
-                                            <img src={index === hover ? "../assets/icons/tabs/Play.svg" : item.icon} data-index={index} />
-                                        }
+                            onClick={openTab}
+                            onMouseEnter={mEnter}
+                            onMouseLeave={mLeave}
+                            data-index={index}
+                        >
+                            <div className="state" data-index={index}>
+                                <div className={`state__icon cursor-pointer ${videoProgress["n" + index] != 1 && playing && index == active ? 'playing' : ''}`}
+                                    data-index={index}>
+                                    <div className="state__icon__overlay" data-index={index}>
+                                        <div className="icon" data-index={index}>
+                                            {/* XXX: Need rewrite */}
+                                            {/* Default Icon */}
+                                            {videoProgress["n" + index] != 1 && index != active &&
+                                                <img src={index === hover ? "../assets/icons/tabs/Play.svg" : item.icon} data-index={index} />}
 
-                                        {/* Click Play  */}
-                                        {videoProgress["n" + index] != 1 && !playing && index == active && hover == index &&
-                                            < img onClick={pauseClick} src="../assets/icons/tabs/Pause.svg" data-index={index} />
-                                        }
-                                        {/* Playing */}
-                                        {videoProgress["n" + index] != 1 && playing && index == active && hover != index &&
-                                            <img src="../assets/icons/tabs/Playing.svg" data-index={index} />
-                                        }
+                                            {/* Click Play  */}
+                                            {videoProgress["n" + index] != 1 && !playing && index == active && hover == index &&
+                                                <img onClick={pauseClick} src="../assets/icons/tabs/Pause.svg" data-index={index} />}
+                                            {/* Playing */}
+                                            {videoProgress["n" + index] != 1 && playing && index == active && hover != index &&
+                                                <img src="../assets/icons/tabs/Playing.svg" data-index={index} />}
 
 
-                                        {/* Playing Hover or Click */}
-                                        {videoProgress["n" + index] != 1 && playing && index == active && hover == index &&
-                                            <img onClick={pauseClick} src="../assets/icons/tabs/Playing.svg" data-index={index} />
-                                        }
-                                        {videoProgress["n" + index] != 1 && !playing && index == active && hover != index &&
-                                            <img src="../assets/icons/tabs/Pause.svg" data-index={index} />
-                                        }
+                                            {/* Playing Hover or Click */}
+                                            {videoProgress["n" + index] != 1 && playing && index == active && hover == index &&
+                                                <img onClick={pauseClick} src="../assets/icons/tabs/Playing.svg" data-index={index} />}
+                                            {videoProgress["n" + index] != 1 && !playing && index == active && hover != index &&
+                                                <img src="../assets/icons/tabs/Pause.svg" data-index={index} />}
 
-                                        {/* Reload */}
-                                        {videoProgress[`n${index}`] == 1 &&
-                                            <img onClick={() => { let obj = videoProgress; obj["n" + index] = 0; setVideoProgress({ ...obj }); setTimeout(pauseClick, 0) }} src="../assets/icons/tabs/Repeat.svg" data-index={index} />
-                                        }
+                                            {/* Reload */}
+                                            {videoProgress[`n${index}`] == 1 &&
+                                                <img onClick={() => { let obj = videoProgress; obj["n" + index] = 0; setVideoProgress({ ...obj }); setTimeout(pauseClick, 0); } } src="../assets/icons/tabs/Repeat.svg" data-index={index} />}
+                                        </div>
                                     </div>
                                 </div>
+                                {/* Hover */}
+                                {videoProgress["n" + index] != 1 && index != active &&
+                                    <span className={`video-text ${index === hover ? '' : 'hidden'}`} data-index={index}>
+                                        Play
+                                    </span>}
+
+                                {/* Playing */}
+                                {videoProgress["n" + index] != 1 && playing && index == active &&
+                                    <span className={` video-text `} data-index={index}>
+                                        Playing
+                                    </span>}
+
+                                {/* Pause */}
+                                {videoProgress["n" + index] != 1 && !playing && index == active &&
+                                    <span className={` video-text `} data-index={index}>
+                                        Pause
+                                    </span>}
+
+                                {/* Reload */}
+                                {videoProgress["n" + index] == 1 &&
+                                    <span className={` video-text `} data-index={index}>
+                                        Replay
+                                    </span>}
                             </div>
-                            {/* Hover */}
-                            {videoProgress["n" + index] != 1 && index != active &&
-                                <span className={`video-text ${index === hover ? '' : 'hidden'}`} data-index={index}>
-                                    Play
-                                </span>
-                            }
-
-                            {/* Playing */}
-                            {videoProgress["n" + index] != 1 && playing && index == active &&
-                                <span className={` video-text `} data-index={index}>
-                                    Playing
-                                </span>
-                            }
-
-                            {/* Pause */}
-                            {videoProgress["n" + index] != 1 && !playing && index == active &&
-                                <span className={` video-text `} data-index={index}>
-                                    Pause
-                                </span>
-                            }
-
-                            {/* Reload */}
-                            {videoProgress["n" + index] == 1 &&
-                                <span className={` video-text `} data-index={index}>
-                                    Replay
-                                </span>
-                            }
-                        </div>
-                        <div className="item__text ml-6" data-index={index}>
-                            <div className="title mb-1.5" data-index={index}>{item.title}</div>
-                            <div className="subtitle" data-index={index}>{item.subtitle}</div>
-                            <div className={`progressbar-block ${playing && index == active ? 'playing' : ''}`} data-index={index}>
-                                {index === active &&
-                                    <Progressbar done={currentProgress} />
-                                }
+                            <div className="item__text ml-6" data-index={index}>
+                                <div className="title mb-1.5" data-index={index}>{item.title}</div>
+                                <div className="subtitle" data-index={index}>{item.subtitle}</div>
+                                <div className={`progressbar-block ${playing && index == active ? 'playing' : ''}`} data-index={index}>
+                                    {index === active && // ALERT: This is the only way to make the progressbar work properly
+                                        <ProgressBar done={currentProgress} /> 
+                                        // FIX: loading fast, need to fix it slow loading progressbar
+                                    }
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
             {<TabContent {...items[active]} defaultItem={items[0]} playing={playing} setCurrentProgress={setCurrentProgress} setPlaying={setPlaying} active={active} videoProgress={videoProgress} setVideoProgress={setVideoProgress} />}
         </div>
