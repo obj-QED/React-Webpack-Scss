76d14e8ece2dd01c5d5eedfe5add96d69314769c
Vladislav <send@valabuev.ru>
2022-01-24 16:16:58 +0400

    small style fixes

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Progressbar({ done }) {
   return (
-    <div className={"progressbar w-full h-1 pointer-events-none mt-4"}>
+    <div className={"progressbar w-full h-1 pointer-events-none mt-4 overflow-hidden"}>
       <div
         className={"progressbar-done h-full"}
         style={{
